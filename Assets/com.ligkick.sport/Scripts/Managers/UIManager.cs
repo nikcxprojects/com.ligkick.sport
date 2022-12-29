de47950a765a5739ee9eb7d3ff0a84bc5e29dc92
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-29 15:42:03 +0300

    update

--- Assets/com.ligkick.sport/Scripts/Managers/UIManager.cs
+++ Assets/com.ligkick.sport/Scripts/Managers/UIManager.cs
@@ -8,11 +8,10 @@ public class UIManager : MonoBehaviour
 
     private GameObject _last = null;
 
+    [SerializeField] GameObject getStarted;
     [SerializeField] GameObject menu;
-    [SerializeField] GameObject options;
     [SerializeField] GameObject top;
     [SerializeField] GameObject game;
-    [SerializeField] GameObject pause;
     [SerializeField] GameObject result;
 
     [Space(10)]
@@ -52,12 +51,11 @@ public class UIManager : MonoBehaviour
 
         switch(windowIndex)
         {
-            case 0: _last = menu; break;
-            case 1: _last = options; break;
+            case 0: _last = getStarted; break;
+            case 1: _last = menu; break;
             case 2: _last = top; break;
             case 3: _last = game; break;
-            case 4: _last = pause;break;
-            case 5: _last = result; break;
+            case 4: _last = result; break;
         }
 
         _last.SetActive(true);
