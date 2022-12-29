75858e32b85e740059753a3f6f663951d6349a87
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-29 17:14:48 +0300

    update

---
+++ Assets/com.ligkick.sport/Scripts/Foot.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Foot : MonoBehaviour
+{
+    private Vector2 InitPosition { get; set; }
+    private Vector2 TargetPosition { get; set; }
+    private Rigidbody2D Rigidbody { get; set; }
+
+    private const float speed = 10.0f;
+
+    private void Awake()
+    {
+        InitPosition = transform.position;
+        Rigidbody = GetComponent<Rigidbody2D>();
+
+        TargetPosition = InitPosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        TargetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        TargetPosition = InitPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 newPosition = Vector2.MoveTowards(Rigidbody.position, TargetPosition, Time.deltaTime * speed);
+        Rigidbody.MovePosition(newPosition);
+    }
+}
